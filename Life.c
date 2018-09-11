9f92245a7be2c9f7f9a8bdb4fc63e1cdd2e5c97e
Mo76 <moghadam0076@yahoo.com>
2018-09-11 20:17:57 +0800

    added the cases and the swap loop

--- Life.c
+++ Life.c
@@ -6,9 +6,9 @@
 
 int  main(int argc, char *args[]){
     
-     int width;
-     int height;
-     time_t t;
+    int width;
+    int height;
+    time_t t;
     double probability;
     if(argc==4){
         width = atoi(args[1]);
@@ -17,6 +17,7 @@ int  main(int argc, char *args[]){
       // printf("w = %d h = %d p= %f",width,height, probability);
     }
     bool map[width][height];
+    bool nextMap[width][height];
     srand((unsigned) time(&t));
     for (int i = 0; i < width; i++){
         for (int j = 0; j < height; j++){
@@ -24,7 +25,7 @@ int  main(int argc, char *args[]){
              map[i][j] = (double)rand()/RAND_MAX < probability;
            }
     }
-    /*print matricx
+   
     for (int i = 0; i < width; i++){
         for (int j = 0; j < height; j++){
              if(map[i][j]) printf("1\t");
@@ -32,8 +33,37 @@ int  main(int argc, char *args[]){
            }
            printf("\n");
     }
-    */
     
-   
 
+   //next Generation
+   for (int p = 0; p < width; p++){
+        for (int q = 0; q < height; q++){
+                switch (0)
+                {
+                    case 2:  nextMap[p][q] = map[p][q]; break;
+                    case 3:  nextMap[p][q] = true;      break;
+                    default: nextMap[p][q] = false;
+                } 
+        }
+   }
+   //swap the maps
+   
+    for(int x = 0;x<width;x++){
+        for(int y=0;y<height;y++){ 
+           bool temp = map[x][y];
+           map[x][y] = nextMap[x][y];
+           nextMap[x][y] = temp;
+    }
 }
+
+ 
+
+    printf("Next generation\n");
+   for (int z = 0; z < width; z++){
+        for (int x = 0; x < height; x++){
+             if(nextMap[z][x]) printf("1\t");
+             if(!nextMap[z][x]) printf("0\t");
+           }
+           printf("\n");
+    }
+}   
